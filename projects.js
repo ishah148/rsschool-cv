5858c03e44468749352548ab759d4a1cf1502d2f
Igor Shah <zombee1992@gmail.com>
2022-06-18 18:48:24 +0300

    feat: add content

--- projects.js
+++ projects.js
@@ -1,15 +1,35 @@
 export const projectsData = [
     {
+        name:'Farba Studio',
+        linkGit:'https://github.com/ishah148/Farba',
+        linkDeploy:'https://ishah148.github.io/Farba/pages/',
+        description:'My first commercial project',
+        imageName:'farba',
+    },
+    {
         name:'Game',
-        linkGit:'https://ishah148.github.io/gitTest/movie',
-        linkDeploy:'https://ishah148.github.io/gitTest/movie',
+        linkGit:'https://github.com/ishah148/gitTest/tree/gh-pages/game',
+        linkDeploy:'https://ishah148.github.io/gitTest/game',
         description:'Educational project from RSS stage 0',
         imageName:'game',
     },
-
+    {
+        name:'Virtual Keyboard',
+        linkGit:'https://github.com/ishah148/Virtual-Keyboard',
+        linkDeploy:'https://ishah148.github.io/Virtual-Keyboard/',
+        description:'Educational project from RSS stage 1',
+        imageName:'keyboard',
+    },
+    {
+        name:'Shelter',
+        linkGit:'https://rolling-scopes-school.github.io/ishah148-JSFE2022Q1/shelter/',
+        linkDeploy:'https://rolling-scopes-school.github.io/ishah148-JSFE2022Q1/shelter/',
+        description:'Educational project from RSS stage 1',
+        imageName:'shelter',
+    },
     {
         name:'Movie-app',
-        linkGit:'https://ishah148.github.io/gitTest/movie',
+        linkGit:'https://github.com/ishah148/gitTest/tree/gh-pages/movie',
         linkDeploy:'https://ishah148.github.io/gitTest/movie',
         description:'Educational project from RSS stage 0',
         imageName:'movie-app',
@@ -21,4 +41,5 @@ export const projectsData = [
         description:'Educational project from RSS stage 0',
         imageName:'portfolio',
     },
+
 ]
