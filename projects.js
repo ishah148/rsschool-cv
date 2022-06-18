a80ca87a38e44d1b89cdcb2ec1939d3dffddc867
Igor Shah <zombee1992@gmail.com>
2022-06-18 18:11:09 +0300

    feat: add config

---
+++ projects.js
@@ -0,0 +1,24 @@
+export const projectsData = [
+    {
+        name:'Game',
+        linkGit:'https://ishah148.github.io/gitTest/movie',
+        linkDeploy:'https://ishah148.github.io/gitTest/movie',
+        description:'Educational project from RSS stage 0',
+        imageName:'game',
+    },
+
+    {
+        name:'Movie-app',
+        linkGit:'https://ishah148.github.io/gitTest/movie',
+        linkDeploy:'https://ishah148.github.io/gitTest/movie',
+        description:'Educational project from RSS stage 0',
+        imageName:'movie-app',
+    },
+    {
+        name:'Portfolio',
+        linkGit:'https://ishah148.github.io/gitTest/movie',
+        linkDeploy:'https://ishah148.github.io/gitTest/movie',
+        description:'Educational project from RSS stage 0',
+        imageName:'portfolio',
+    },
+]
